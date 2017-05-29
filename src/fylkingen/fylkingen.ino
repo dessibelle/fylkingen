d68f7d8fba51e8dd2b673c075d79a4b7a0e607c9
Simon Fransson <simon@funnel.io>
2017-05-29 09:30:49 +0200

    Use the F() macro to store static strings in Flash memory rather than program memory.

--- src/fylkingen/fylkingen.ino
+++ src/fylkingen/fylkingen.ino
@@ -12,7 +12,7 @@ double sessionPressureBaseline;
 /*********************************
  * BMP180 Barometic sensor setup *
  *********************************/
- 
+
 SFE_BMP180 barometer; // BPM180 pressure struct
 #define BAROMETER_ALTITUDE 52.1 // Altitude of Stockholm, Sweden in meters
 #define BAROMETER_BASELINE_PRESSURE 1013.25 // "Standard" pressure, the baseline used universally, is 1013.25 hPa
@@ -21,7 +21,7 @@ SFE_BMP180 barometer; // BPM180 pressure struct
 /***********************************
  * HC-SR04 Ultrasound sensor setup *
  ***********************************/
- 
+
 /*
  * HC-SR04 Ultrasound / Ping distance sensor
  * https://cdn.sparkfun.com/datasheets/Sensors/Proximity/HCSR04.pdf
@@ -33,32 +33,32 @@ SFE_BMP180 barometer; // BPM180 pressure struct
  * 1. Using IO trigger for at least 10µs high level signal,
  * 2. The Module automatically sends eight 40 kHz and detect whether there is a pulse signal back.
  * 3. IF the signal back, through high level, time of high output IO duration is the time from
- *    sending ultrasonic to returning. Test distance = (high level time×velocity of sound (340M/S) / 2, 
+ *    sending ultrasonic to returning. Test distance = (high level time×velocity of sound (340M/S) / 2,
  */
 #define ULTRASOUND_TRIG_PIN 13
 #define ULRTASOUND_ECHO_PIN 12
-#define ULTRASOUND_UPPER_BOUNDS 2000 // Theoretically 4 metres, but 2 works better 
+#define ULTRASOUND_UPPER_BOUNDS 2000 // Theoretically 4 metres, but 2 works better
 #define ULTRASOUND_LOWER_BOUNDS 20
 
 
 /*************************************
  * BMP180 Barometic sensor functions *
  *************************************/
- 
+
 void setupBarometricSensor()
 {
   if (barometer.begin())
   {
-    Serial.print("BMP180 init success, altitude: ");
+    Serial.print(F("BMP180 init success, altitude: "));
     Serial.print(BAROMETER_ALTITUDE);
-    Serial.print(", oversampling mode: ");
+    Serial.print(F(", oversampling mode: "));
     Serial.println(BAROMETER_OVERSAMPLING_MODE);
   } else
   {
     // Oops, something went wrong, this is usually a connection problem
-    Serial.println("BMP180 init fail\n\n");
+    Serial.println(F("BMP180 init fail\n\n"));
     while(1); // Pause forever.
-  }  
+  }
 }
 
 
@@ -75,11 +75,11 @@ short getTemeprature(double &temperature)
     status = barometer.getTemperature(temperature);
     if (status != 1)
     {
-      Serial.println("error retrieving temperature measurement\n");
+      Serial.println(F("Error retrieving temperature measurement\n"));
     }
   } else
   {
-    Serial.println("error starting temperature measurement\n");
+    Serial.println(F("Error starting temperature measurement\n"));
   }
   return status;
 }
@@ -94,16 +94,17 @@ short getPressure(double &pressure, double temperature, char oversamplingMode)
   // Start a pressure measurement using the specified oversamplingMode, from 0 to 3 (highest res, longest wait).
   // If request is successful, the number of ms to wait is returned. Otherwise  0 is returned.
   char measurementDelay = barometer.startPressure(oversamplingMode);
-  if (measurementDelay > 0) {
+  if (measurementDelay > 0)
+  {
     delay(measurementDelay); // Wait for the measurement to complete
     status = barometer.getPressure(pressure, temperature);
     if (status != 1)
     {
-      Serial.println("error retrieving pressure measurement\n");
+      Serial.println(F("Error retrieving pressure measurement\n"));
     }
   } else
   {
-    Serial.println("error starting pressure measurement\n");
+    Serial.println(F("Error starting pressure measurement\n"));
   }
   return status;
 }
@@ -111,8 +112,8 @@ short getPressure(double &pressure, double temperature, char oversamplingMode)
 /*
  * The pressure sensor returns abolute pressure, which varies with altitude.
  * To remove the effects of altitude, use the sealevel function and your current altitude.
- * This number is commonly used in weather reports. 
- * 
+ * This number is commonly used in weather reports.
+ *
  * Returns the sealevel compensated pressure in milli bar
  */
 double getSealevelPressure(double pressure, double altitude)
@@ -131,7 +132,7 @@ double getAltitude(double absolutePressure, double pressureBaseline)
 /********************************
  * HC-SR04 Ultrasound functions *
  ********************************/
- 
+
 void setupUltrasound(short trigPin, short echoPin) {
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
@@ -148,14 +149,14 @@ void HCSR04_do_trig(short trigPin) {
 
 /*
  * Returns the ping duration of the HC-SR04 ultrasound sensor
- * 
+ *
  *  Formula: uS / 58 = centimeters or uS / 148 =inch; or: the
  *  range = high level time * velocity (340M/S) / 2; we suggest to use over 60ms
  *  measurement cycle, in order to prevent trigger signal to the echo signal
  */
 long getUltrasoundPingDuration(short trigPin, short echoPin)
 {
-  HCSR04_do_trig(trigPin);  
+  HCSR04_do_trig(trigPin);
   return pulseIn(echoPin, HIGH);
 }
 
@@ -180,7 +181,7 @@ void setup()
   setupBarometricSensor();
   setupUltrasound(ULTRASOUND_TRIG_PIN, ULRTASOUND_ECHO_PIN);
 
-  Serial.println("\n--------\n");
+  Serial.println(F("\n--------\n"));
 }
 
 void loop() {
@@ -196,32 +197,33 @@ void loop() {
   }
   pressureDifferential = sealevelPressure - sessionPressureBaseline;
 
-  Serial.print("Temperature: ");
+  Serial.print(F("Temperature: "));
   Serial.print(temperature, 2);
-  Serial.println("°C");
-  Serial.print("Pressure (absolute): ");
+  Serial.println(F("°C"));
+  Serial.print(F("Pressure (absolute): "));
   Serial.print(pressure, 2);
-  Serial.println(" mbar");
-  Serial.print("Pressure (sealevel): ");
+  Serial.println(F(" mbar"));
+  Serial.print(F("Pressure (sealevel): "));
   Serial.print(sealevelPressure, 2);
-  Serial.println(" mbar");
-  Serial.print("Altitude: ");
+  Serial.println(F(" mbar"));
+  Serial.print(F("Altitude: "));
   Serial.print(altitude, 2);
-  Serial.println(" m");
-  Serial.print("Pressure differential: ");
-  Serial.print(pressureDifferential, 2);  
-  Serial.println(" mbar");
-  
+  Serial.println(F(" m"));
+  Serial.print(F("Pressure differential: "));
+  Serial.print(pressureDifferential, 2);
+  Serial.println(F(" mbar"));
+
   long distance = getUltrasoundPingDistance(ULTRASOUND_TRIG_PIN, ULRTASOUND_ECHO_PIN);
-  if (distance >= ULTRASOUND_UPPER_BOUNDS || distance <= ULTRASOUND_LOWER_BOUNDS){
-    Serial.println("Out of range");
-  }
-  else { 
-    Serial.print("Distance: ");
+  if (distance >= ULTRASOUND_UPPER_BOUNDS || distance <= ULTRASOUND_LOWER_BOUNDS)
+  {
+    Serial.println(F("Out of range"));
+  } else
+  {
+    Serial.print(F("Distance: "));
     Serial.print(distance);
-    Serial.println(" mm");
+    Serial.println(F(" mm"));
   }
 
-  Serial.println("\n--------\n");
+  Serial.println(F("\n--------\n"));
   delay(SAMPLING_DELAY);
 }
